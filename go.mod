cbb465bd8bb76858b11ac8d6a626ade4a49beed9
Hector A. Medina Torres <hectormedina1274@gmail.com>
2025-02-08 13:33:41 -0500

    Calculate age. Added logic to receive a GET request with the user's birth date and logic to reply with a JSON containing the age.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module age-calculator/backend
 
 go 1.23.5
+
+require github.com/gorilla/schema v1.4.1
--- main.go
+++ main.go
@@ -1,15 +1,67 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+	"time"
+	"github.com/gorilla/schema"
 )
 
-func main() {
-	http.HandleFunc("/", Handler)
-	http.ListenAndServe(":3432", nil)
+type Date struct {
+	Day int `schema:"day"`
+	Month int `schema:"month"`
+	Year int `schema:"year"`
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!\n")
+type AgeCalcResponse struct {
+	Age int
+}
+
+func HandleCalcAge(requestWriter http.ResponseWriter, request *http.Request) {
+	requestWriter.Header().Set("Content-Type", "application/json")
+	requestWriter.WriteHeader(http.StatusCreated)
+
+	// Validate that the URL path is correct
+	if request.URL.Path != "/age-calc" {
+		http.Error(requestWriter, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	// Populate request form
+	if err := request.ParseForm(); err != nil {
+		
+    }
+
+    // Create an empty Date
+    birthDate := new(Date)
+    ageResponse := new(AgeCalcResponse)
+
+    // Decode Form into Date struct
+    if err := schema.NewDecoder().Decode(birthDate, request.Form); err != nil {
+        http.Error(requestWriter, "422 Invalid Query.", http.StatusUnprocessableEntity)
+		return
+    }
+
+    birthDateObj := time.Date(birthDate.Year, time.Month(birthDate.Month), birthDate.Day, 0, 0, 0, 0, time.UTC)
+    ageResponse.Age = calculateAge(birthDateObj)
+
+    json.NewEncoder(requestWriter).Encode(ageResponse)
+}
+
+// Calculates age by comparing provided date with input age
+func calculateAge(birthDate time.Time) int {
+	var currentDate time.Time = time.Now().UTC()
+
+	var age int = int(currentDate.Sub(birthDate).Hours()/24/365)
+
+	if age < 0 || age > 292 {
+		age = 0
+	}
+
+	return age
+}
+
+func main() {
+	http.HandleFunc("/age-calc", HandleCalcAge)
+	http.ListenAndServe(":3432", nil)
 }
